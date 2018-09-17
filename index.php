7510c1df92071aa5060bc3867f9bd4d29fab0939
chinguboss <38854597+chinguboss@users.noreply.github.com>
2018-09-17 11:20:54 +0900

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<? include ("header.php"); ?>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="http://rochestb.github.io/jQuery.YoutubeBackground/src/jquery.youtubebackground.js"></script>
+  <div id="video" class="background-video"></div>
+  <script>
+    $('#video').YTPlayer({
+      fitToBackground: true,
+      videoId: 'LSmgKRx5pBo'
+    });
+  </script>
+
+  <div class='container'>
+    <p align="center"><img src="images/banner.PNG" width="100%"></p>
+    <h1>K-Mall</h1>
+    <p>전세계 250여개국의 상품 중 최고의 제품을 선정하여 적절한 가격에 제공하는 신개념 소셜 커머스입니다.</br>
+    <b>바로 오늘 시작하세요!</b></p>
+    <p class="ref">본 사이트에서 사용된 리소스는 학술적 용도로만 사용되었으며, 상업적 사용은 제한됩니다.
+        *1 : 배경 youtube 동영상의 모든 권리는 <a href="https://www.youtube.com/watch?v=LSmgKRx5pBo">
+        Joel Cosson</a>에 있습니다. *2 : 배너 이미지의 모든 권리는 <a href="vitatra.de">vitatra</a>에 있습니다.
+        *3 : youtube background player의 모든 권리는 <a href="https://github.com/rochestb/jQuery.YoutubeBackground">rochestb</a>에 있습니다.</p>
+  </div>
+<? include ("footer.php"); ?>
