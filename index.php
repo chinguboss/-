8115645bff0e0a8fed72ef4cd92ccedc1bc59082
chinguboss <38854597+chinguboss@users.noreply.github.com>
2018-09-17 11:23:16 +0900

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,37 @@
       videoId: 'LSmgKRx5pBo'
     });
   </script>
+  
+  <style>
+    .background-video {
+      background-position: top center;
+      background-repeat: no-repeat;
+      bottom: 0;
+      left: 0;
+      overflow: hidden;
+      position: fixed;
+      right: 0;
+      top: 0;
+    }
+    .navbar {
+      z-index:999;
+    }
+    h1, p {
+      padding: 0px 30px 0px 30px;
+      text-align:center;
+    }
+    h1 {
+      font-weight:800;
+    }
+    .container {
+      position: relative;
+      background: rgba(255, 255, 255, .9);
+    }
+    .ref {
+      font-weight:200;
+      font-size:0.5em;
+    }
+  </style>
 
   <div class='container'>
     <p align="center"><img src="images/banner.PNG" width="100%"></p>
