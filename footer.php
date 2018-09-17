dd6276ea293fdcb90bd2cfcf7f536ab2cc73611a
chinguboss <38854597+chinguboss@users.noreply.github.com>
2018-09-17 11:19:01 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+  <div class="container">
+    <div id="footer">
+      Copyright (c) <a href="http://intelligence.korea.ac.kr">
+      intelligence.korea.ac.kr</a> All rights reserved.
+    </div>
+  </body>
+</html>
